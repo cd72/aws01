b4f90e4b8eb482dd968bb04a645d902323ce0362
cd72 <chris.j.denney@gmail.com>
2021-09-07 20:12:22 +0100

    added main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0194c3e07668a7e36"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
